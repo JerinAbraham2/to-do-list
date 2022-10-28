1661863de624b6dfbaddd55658521b73e28c56e3
Ethan <et.l.nicholas@gmail.com>
2022-10-29 10:41:03 +1100

    add javascript in to structure

---
+++ script.js
@@ -0,0 +1,15 @@
+function openTheForm() {
+    document.getElementById("popupForm").style.display = "block";
+  }
+  
+  function closeTheForm() {
+    document.getElementById("popupForm").style.display = "none";
+  }
+
+  function openCardsPanel() {
+    document.getElementById("cards").style.display = "block";
+  }
+  
+  function closeCardsPanel() {
+    document.getElementById("cards").style.display = "none";
+  }
