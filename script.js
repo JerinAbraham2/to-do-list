56a58e3945ca109ad16a45720eea12ea2cf5f06a
Ethan <et.l.nicholas@gmail.com>
2022-10-31 09:36:14 +1100

    temprate saving to update main

--- script.js
+++ script.js
@@ -14,34 +14,58 @@ function openTheForm() {
     document.getElementById("main-section").style.display = "none";
 }
 
-  function validationForm() {
-    // Get the values of the input field with id=""
-    let name        = document.getElementById("name").value;
-    let description = document.getElementById("description").value;
-    let assignTo    = document.getElementById("assignto").value;
-    let dueDay      = document.getElementById("due").value;
-    let option      = document.getElementById("satus").value;
 
 
-    // If name is Not a letter or less than 3 charactors or greater than 100 charactors
-    let nameRegex = /^[a-zA-Z]+(?:-[a-zA-Z]+)*$/;
-    if (name.match(nameRegex) &&  2 < name.length < 101) {
-        text = "Input success";
-        closeTheForm();
-        openCardsPanel();
-    } else {
-        text = "Requres name";
-    }
-    document.getElementById("demo").innerHTML = text;
+(function () {
+  'use strict'
 
-    let desciptRegex = /^[a-zA-Z]+(?:-[a-zA-Z]+)*$/;
-    if (name.match(nameRegex) &&  2 < name.length < 101) {
-        text = "Input success";
-        closeTheForm();
-        openCardsPanel();
-    } else {
-        text = "Requres name";
-    }
-    document.getElementById("demo").innerHTML = text;
-}
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
+
+
+
+//   function validationForm() {
+//     // Get the values of the input field with id=""
+//     let name        = document.getElementById("name").value;
+//     let description = document.getElementById("description").value;
+//     let assignTo    = document.getElementById("assignto").value;
+//     let dueDay      = document.getElementById("due").value;
+//     let option      = document.getElementById("satus").value;
+
+
+//     // If name is Not a letter or less than 3 charactors or greater than 100 charactors
+//     let nameRegex = /^[a-zA-Z]+(?:-[a-zA-Z]+)*$/;
+//     if (name.match(nameRegex) &&  2 < name.length < 101) {
+//         text = "Input success";
+//         closeTheForm();
+//         openCardsPanel();
+//     } else {
+//         text = "Requres name";
+//     }
+//     document.getElementById("demo").innerHTML = text;
+
+//     let desciptRegex = /^[a-zA-Z]+(?:-[a-zA-Z]+)*$/;
+//     if (name.match(nameRegex) &&  2 < name.length < 101) {
+//         text = "Input success";
+//         closeTheForm();
+//         openCardsPanel();
+//     } else {
+//         text = "Requres name";
+//     }
+//     document.getElementById("demo").innerHTML = text;
+// }
 
