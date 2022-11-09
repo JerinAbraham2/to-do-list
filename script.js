71932709bac1d379772977e5a4fbec7d213dee0c
tobi turner <tobi@something.com>
2022-11-09 14:34:49 +1100

    Revert "DCT-6 fix the validation for each checkbox"
    This reverts commit be97b13fc20f15951972611eacd93dba9cf4ca8f.

--- script.js
+++ script.js
@@ -16,25 +16,81 @@ function closeCardsPanel() {
 }
 
 
-// Checking each person's checkbox
-let persons = document.getElementsByName('person');
-Array.from(persons).forEach((element)=>{
-  element.addEventListener('change',validate);
-})
+function checkAssignTo() {
+  let button1 = document.getElementById("person1").onclick;
+  let button2 = document.getElementById("person2").onclick;
+  let button3 = document.getElementById("person3").onclick;
+  let button4 = document.getElementById("person4").onclick;
+  console.log(button1.toString());
+  console.log(button2.toString());
+  console.log(button3.toString());
+  console.log(button4.toString());
+}
+
+// // Bind function to onclick event for checkbox
+// document.getElementById('agree').onclick = function() {
+//   // access properties using this keyword
+//   if ( this.checked ) {
+//       // Returns true if checked
+//      
+//   } else {
+//       // Returns false if not checked
+//   }
+// };
+
+/*window.onload=function(){
+  alert("windows load")
+document.getElementById('btn-group').addEventListener('change',confirm,false);
+}*/
+
+var checkboxes = document.querySelectorAll('input[type="checkbox"]');
+var checkbox =document.getElementById('btn-group');
+// var submit_btn=document.getElementById("submit-button");
+
+function confirm(){
+ 
+  checkboxes.forEach(function(entry) {
+  //var checkbox=document.getElementById('btn-group');
+ 
+  if(checkbox.checked){
+    
+    document.getElementById("chk_option_error").style.visibility = "hidden"
+    document.getElementById("chk_option_ok").style.visibility = "visible"
+  }
+   else if(checkbox.checked==false){
+      document.getElementById("chk_option_error").style.visibility = "visible"
+      document.getElementById("chk_option_ok").style.visibility = "hidden"
+    }
+  
+}
+  );
+}
+
+checkboxes.forEach(function(entry) {
+  entry.onchange = confirm; // Bind confirm() function on change of each checkboxes
+  });
+
+
+
+function validate() {
+  document.getElementById('termsChkbx').addEventListener('click', function (e) {
+  document.getElementById('sub1').disabled = !e.target.checked;
+  });
+} 
+
 
 
 /*seperate function validate() to validate the checkbox button group*/
-// adopt the validate function for each individual person.
 function validate() {
 
   var form_data = new FormData(document.querySelector("form"));
-  if (!form_data.has("person")) //checking for the name person[] is present from the checkbox buttons
+  if (!form_data.has("person[]")) //checking for the name person[] is present from the checkbox buttons
   {
     document.getElementById("chk_option_error").style.visibility = "visible"; //display invalid feedback if the checkbox isn't checked
     document.getElementById("chk_option_ok").style.visibility = "hidden"; //hide the positive feedback 
     return false;
   }
-  else //if the form has person
+  else //if the form has person[]
   {
     document.getElementById("chk_option_error").style.visibility = "hidden"; //hide the invalid feedback
     document.getElementById("chk_option_ok").style.visibility = "visible"; //display the positive feedback
@@ -65,11 +121,12 @@ function validate() {
           event.preventDefault()
           event.stopPropagation()
         }
-
+        //validate()
       }, false)
     })
 })()
 
+<<<<<<< HEAD
 // Validation for forms with bootstrap classes.
 (function () {
   'use strict'
@@ -112,3 +169,6 @@ function checkAssignTo() {
   }
 return true;
 }
+=======
+
+>>>>>>> parent of be97b13 (DCT-6 fix the validation for each checkbox)
