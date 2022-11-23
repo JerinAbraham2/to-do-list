34987a619e9efc71738aeae43920edb378dd6fe9
Ethan <et.l.nicholas@gmail.com>
2022-11-23 15:34:58 +1100

    save the change

--- script.js
+++ script.js
@@ -65,3 +65,10 @@ function validateAssign() {
       }, false)
     })
 })()
+
+document.getElementById("dateInput").addEventListener("submit", function() {
+  var input = this.value;
+  var dateEntered = new Date(input);
+  console.log(input); //e.g. 2015-11-13
+  console.log(dateEntered); //e.g. Fri Nov 13 2015 00:00:00 GMT+0000 (GMT Standard Time)
+});
