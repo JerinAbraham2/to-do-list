d4cb6aa38126ff5bb8030e8d777198f78447cc94
Ethan <et.l.nicholas@gmail.com>
2022-11-23 23:50:35 +1100

    format the functions and structures on validate.js

--- validate.js
+++ validate.js
@@ -1,168 +1,132 @@
+// Click create task : open the form and close the main-section on html
 const openTheForm = () => {
     document.getElementById("form-id").style.display = "flex";
     document.getElementById("main-section").style.display = "none";
-  };
-  
-  
-  
-  
-  const longString = 'Create a javascript fileOne of those farmers is Brian Barry, a grain grower south of Manangatang' +
-    'who now faces a 120-kilometre detour to access the local Graincorp that is just 10km away He said the closure of the' +
-    'Robinvale-Sea Lake Road was creating a lot of problems.s a major transport route for' +
-    'grain and irrigat ruit and vegetables out of the Robinvale-Wemen area, Mr Barry said.' +
-    'Create a javascript fileOne of those farmers is Brian Barry, a grain grower south of Manangatang' +
-    'who now faces a 120-kilometre detour to access the local Graincorp that is just 10km away He said the closure of the' +
-    'Robinvale-Sea Lake Road was creating a lot of problems.s a major transport route for' +
-    'grain and irrigat ruit and vegetables out of the Robinvale-Wemen area, Mr Barry said.'
-  
-  const validateName = (taskName) => {
-  
-    if (typeof (taskName) !== 'string') {
-      return 'Please insert text for your task.'
-  
+};
+
+// validateString function to validate the "TaskName" and "Description"
+// Parameters with 1. form input.  2.expected dataType, 3 minimum length of input string 
+// 4 maximum length of input string .
+// Function return with reulst object that cotains Boolean values and feed back string.
+const validateString = (input, dataType, minLength, maxLength) => {
+
+    let result = { status: false, feedback: '' };
+    if (typeof (input) !== dataType) {
+        result.status = false
+        result.feedback = 'Please insert text for your task.'
+        return result;
     }
-    if (taskName.length <= 8) {
-      return 'Please insert text over 8 characters.'
+    if (input.length <= minLength) {
+        result.status = false;
+        result.feedback = `Please insert text over ${minLength} characters.`;
+        return result;
     }
-    if (taskName.length > 200) {
-      return 'Please insert text less than 200 characters.'
+    if (input.length > maxLength) {
+        result.status = false;
+        result.feedback = `Please insert text less than ${maxLength} characters.`;
+        return result;
     }
-    return "The format is good!"
-  };
-  
-  const validateDesc = (Desc) => {
-  
-    if (typeof (Desc) !== 'string') {
-      return 'Please insert description text for your task.'
-  
-    }
-    if (Desc.length <= 15) {
-      return 'Please insert text over 15 characters.'
-    }
-    if (Desc.length > 300) {
-      return 'Please insert text less than 200 characters.'
-    }
-  
-    return "The format is good!";
-  };
-  
-  
-  const validateAssignedTo = (Assigned) => {
-  
-    if (typeof (Assigned) !== 'string') {
-      return 'Please insert description text for your task.'
-    }
-    if (Assigned.length <= 8) {
-      return 'Please insert longer text over 8 characters.'
-    }
-    if (Assigned.length > 200) {
-      return 'Please insert longer text less than 200 characters.'
-    }
-    return "The format is good!";
-  };
-  
-  const validateDueDate = (Duedate, currDate) => {
-  
-    if (typeof (Assigned) !== 'Date') {
-      return 'Please insert text for your task.'
-  
-    }
-    if (Assigned.length <= currDate) {
-      return 'Please insert longer text over 8 characters.'
-    }
-  
-    return "The format is good!";
-  };
-  
-  // Assign validation
-  
-  // Checking each person's checkbox
-  const checkAssignChange = () => {
-    let persons = document.getElementsByName('person');
-    Array.from(persons).forEach((element) => {
-      element.addEventListener('change', validateAssign);
-    })
-  }
-  
-  /*seperate function validate() to validate the checkbox button group*/
-  // adopt the validate function for each individual person.
-  function validateAssign() {
-    var form_data = new FormData(document.querySelector("form"));
-    if (!form_data.has("person")) //checking for the name person[] is present from the checkbox buttons
-    {
-      document.getElementById("chk_option_error").style.visibility = "visible"; //display invalid feedback if the checkbox isn't checked
-      document.getElementById("chk_option_ok").style.visibility = "hidden"; //hide the positive feedback 
-      return false;
-    }
-    else //if the form has person
-    {
-      document.getElementById("chk_option_error").style.visibility = "hidden"; //hide the invalid feedback
-      document.getElementById("chk_option_ok").style.visibility = "visible"; //display the positive feedback
-    }
-    return true;
-  }
-  
-  
-  
-  const validateEachInput = (validatedString, input) => {
-    if (validatedString === "The format is good!") {
-      document.getElementById(`valid-${input}`).style.display = "block";
-      document.getElementById(`valid-${input}`).innerHTML = validatedString;
-      document.getElementById(`invalid-${input}`).style.display = "none";
-    } else {
-      document.getElementById(`valid-${input}`).style.display = "none";
-      document.getElementById(`invalid-${input}`).style.display = "block";
-      document.getElementById(`invalid-${input}`).innerHTML = validatedString;
-    }
-  }
-  
-  
-  const validateForm = (e) => {
-    e.preventDefault();
-    // name
-    const formName = document.getElementById("name");
-    validateEachInput(validateName(formName.value), "name");
-    // description
-    const formDesc = document.getElementById("description");
-    validateEachInput(validateDesc(formDesc.value), "desc");
-  
-    // assign to
-    checkAssignChange();
-    validateAssign()
-  
-    // date
+    result.status = true;
+    result.feedback = "The format is good!";
+    return result;
+};
+
+// Separation of concern for current date processing
+// return a formated current date string:"YYYY-MM-DD".
+const processCurrentDate = () => {
     const today = new Date();
     var dd = today.getDate();
     var mm = today.getMonth() + 1;
     var yyyy = today.getFullYear();
     currentDate = `${yyyy}-${mm}-${dd}`;
-  
+    return currentDate
+}
+
+// Function for the Validatation of the DueDate
+// Parameter: currentDate with format "YYYY-MM-DD".  
+// Function return with reulst object that cotains Boolean values and feed back string. 
+const validateDate = (currDate) => {
     const selectedDate = document.getElementById("due-date").value;
-  
-    if (selectedDate >= currentDate) {
-      validateEachInput("Looks good", "date")
+    let result = { status: false, feedback: '' };
+    if (selectedDate >= currDate) {
+        result.status = true;
+        result.feedback = "The date is valid.";
+        return result;
     } else {
-      validateEachInput("Cannot set that date", "date")
+        result.status = false;
+        result.feedback = "Cannot set the date earlier than today";
+        return result;
     }
-  
-  
-  };
-  const formEl = document.getElementById("taskform");
-  formEl.addEventListener("submit", validateForm)
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  // document.getElementById("dateInput").addEventListener("submit", function() {
-  //     var input = this.value;
-  //     var dateEntered = new Date(input);
-  //     console.log(input); //e.g. 2015-11-13
-  //     console.log(dateEntered); //e.g. Fri Nov 13 2015 00:00:00 GMT+0000 (GMT Standard Time)
-  // });
-  
+};
+
+
+// Assign validation
+// Checking the change of each person's checkbox and call function 
+//validateAssign() to check at least one person is checked.
+//validateAssign function alone doesn't change the feedback when unchecking 
+// all persons.
+const checkAssignChange = () => {
+    let persons = document.getElementsByName('person');
+    Array.from(persons).forEach((element) => {
+        element.addEventListener('change', validateAssign);
+    })
+}
+
+//the validateAssign function only check at least one person click and offerfeedback.
+// doesn't update the change. requires the assistence from function checkAssignChange().
+function validateAssign() {
+    var form_data = new FormData(document.querySelector("form"));
+    if (!form_data.has("person")) //checking for the name person[] is present from the checkbox buttons
+    {
+        document.getElementById("chk_option_error").style.visibility = "visible"; //display invalid feedback if the checkbox isn't checked
+        document.getElementById("chk_option_ok").style.visibility = "hidden"; //hide the positive feedback 
+        return false;
+    }
+    else //if the form has person
+    {
+        document.getElementById("chk_option_error").style.visibility = "hidden"; //hide the invalid feedback
+        document.getElementById("chk_option_ok").style.visibility = "visible"; //display the positive feedback
+    }
+    return true;
+};
+
+// render the feedback from validate to the html for "TaskName", "Descritpion", "DueDate".
+// Parameter:result Object. This contains result.status:(true or false), result.feedback: feedback string.
+const renderFeedback = (result, idName) => {
+    if (result.status === true) {
+        document.getElementById(`valid-${idName}`).style.display = "block";
+        document.getElementById(`valid-${idName}`).innerHTML = result.feedback;
+        document.getElementById(`invalid-${idName}`).style.display = "none";
+    } else {
+        document.getElementById(`valid-${idName}`).style.display = "none";
+        document.getElementById(`invalid-${idName}`).style.display = "block";
+        document.getElementById(`invalid-${idName}`).innerHTML = result.feedback;
+    }
+};
+
+// a major function to validate forms. calling other fucntion to validate each individual
+// category: "TaskName", "Description", "AssignTO", "DueDate" and update with the validate
+// feedback
+
+const validateForm = (e) => {
+    e.preventDefault();
+        // Task Name
+    const formName = document.getElementById("taskName");
+    renderFeedback(validateString(formName.value, 'string', 8, 100), "taskName");
+    // description
+    const formDesc = document.getElementById("description");
+    renderFeedback(validateString(formDesc.value, 'string', 15, 200), "desc");
+
+    // assign to
+    checkAssignChange();
+    validateAssign();
+
+    // date
+
+    renderFeedback(validateDate(processCurrentDate()), 'date');
+
+
+};
+
+const formEl = document.getElementById("taskform");
+formEl.addEventListener("submit", validateForm);
