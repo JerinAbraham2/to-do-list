2102ad13e420fdf2e1f04acf1b9f5bb304ed1313
Umapathysarma Prathaban <prathaprasan@gmail.com>
2022-11-24 15:29:18 +1100

    completed task6

---
+++ TaskManager.js
@@ -0,0 +1,22 @@
+
+//const uid = (() => (id = 0, () => id++))();
+
+export default class TaskManager{
+    constructor(){
+       this.tasks=[];
+    }
+
+    
+    getAllTasks(){
+
+    }
+    addTask(task){
+
+    }
+    getTasksWithStatus(status){
+
+    }
+   
+}
+
+//export default TaskManager;
